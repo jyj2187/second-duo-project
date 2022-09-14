0682aaa9f0b132f9ce03ea815e3b60ce0dce7c00
jyj2187 <jyjgom@gmail.com>
2022-09-14 11:13:16 +0900

    Initial commit

---
+++ src/main/java/kr/rhinitis/secondduoproject/SecondDuoProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.rhinitis.secondduoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondDuoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondDuoProjectApplication.class, args);
+    }
+
+}
